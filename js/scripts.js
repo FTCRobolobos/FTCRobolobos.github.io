43c4a6bc5ad2d42550a5869d7c954f6760c5a7d4
Wmatistic <Johnmatistic@gmail.com>
2022-06-08 02:15:03 -0500

    Added team bios on hover

--- js/scripts.js
+++ js/scripts.js
@@ -56,6 +56,13 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    
+    function toggleText() {
+        var text = document.getElementById("demo");
+        if (text.style.display === "none") {
+          text.style.display = "block";
+        } else {
+          text.style.display = "none";
+        }
+    }
 
 });
