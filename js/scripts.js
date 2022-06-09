14d534c16c590d7492d8d90120ad2e10e1c15bed
Wmatistic <Johnmatistic@gmail.com>
2022-06-08 23:57:49 -0500

    centered text over team images

--- js/scripts.js
+++ js/scripts.js
@@ -56,13 +56,4 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    function toggleText() {
-        var text = document.getElementById("demo");
-        if (text.style.display === "none") {
-          text.style.display = "block";
-        } else {
-          text.style.display = "none";
-        }
-    }
-
 });
